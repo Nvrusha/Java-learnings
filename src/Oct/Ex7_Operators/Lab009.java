f456950e2eb27c88d797cdaf541f7d1c8b7d343d
Git tutorial <vishalnivangune14.vn@gmail.com>
2024-10-24 07:32:15 +0530

    New

--- src/Oct/Ex7_Operators/Lab009.java
+++ src/Oct/Ex7_Operators/Lab009.java
@@ -2,7 +2,58 @@ package Oct.Ex7_Operators;
 
 public class Lab009 {
     public static void main(String[] args) {
-        System.out.println("You answer is " + 10%12);
+//        1. Arithmetic Operators
+//        + : Addition
+//                - : Subtraction
+//                * : Multiplication
+//                / : Division
+//                % : Modulus (remainder)
+//        2. Unary Operators
+//                 + : Unary plus (positive)
+//                 - : Unary minus (negative)
+//                ++ : Increment (pre-increment/post-increment)
+//                -- : Decrement (pre-decrement/post-decrement)
+//                 ! : Logical NOT
+//        3. Relational (Comparison) Operators
+//                == : Equal to
+//                != : Not equal to
+//                > : Greater than
+//                < : Less than
+//                >= : Greater than or equal to
+//                <= : Less than or equal to
+//        4. Logical Operators
+//                && : Logical AND
+//                || : Logical OR
+//                 ! : Logical NOT
+//        5. Bitwise Operators
+//                 & : Bitwise AND
+//                 | : Bitwise OR
+//                 ^ : Bitwise XOR
+//        ~ : Bitwise complement
+//                << : Left shift
+//                >> : Right shift
+//               >>> : Unsigned right shift
+//        6. Assignment Operators
+//                 = : Simple assignment
+//                += : Add and assign
+//                -= : Subtract and assign
+//                *= : Multiply and assign
+//                /= : Divide and assign
+//                %= : Modulus and assign
+//                &= : Bitwise AND and assign
+//                |= : Bitwise OR and assign
+//                ^= : Bitwise XOR and assign
+//               <<= : Left shift and assign
+//               >>= : Right shift and assign
+//              >>>= : Unsigned right shift and assign
+//        7. Conditional (Ternary) Operator
+//                ?: : condition ? trueExpression : falseExpression
+//        8. Instanceof Operator
+//        instanceof : Checks if an object is an instance of a class.
+//        9. Type Casting Operators
+//                (type) : Used to convert one type into another.
+//        10. String Concatenation Operator
+//                + : Also used for concatenating strings.
 
         // Ternary operator
         int a = 20;
