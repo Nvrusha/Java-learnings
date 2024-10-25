374b94b93943ef5f5cf5c0c6772e64fb41f9e075
Git tutorial <vishalnivangune14.vn@gmail.com>
2024-10-25 23:10:57 +0530

    New

--- src/Oct/Ex7_Operators/Task15_Oct.java
+++ src/Oct/Ex7_Operators/Task15_Oct.java
@@ -2,11 +2,13 @@ package Oct.Ex7_Operators;
 
 public class Task15_Oct {
     public static void main(String[] args) {
-        int a = 20;
+        int a = 500;
         int b = 300;
-        int c = 80;
+        int c = 800;
 
-        String result = a > b ? "A is greater than B" : b > c ? "B is greater than C": "C is greater than B" ;
+        String result = (a > b && a > c) ? "A is the greatest" : (b > a && b > c) ? "B is the greatest":
+                "C is the greatest" ;
+
         System.out.println(result);
     }
 }
