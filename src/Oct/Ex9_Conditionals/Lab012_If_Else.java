49a1157548dd665f3442af53ac3f76542a1539aa
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-10-27 16:33:54 +0530

    New code implemented

---
+++ src/Oct/Ex9_Conditionals/Lab012_If_Else.java
@@ -0,0 +1,21 @@
+package Oct.Ex9_Conditionals;
+
+import java.util.Scanner;
+
+public class Lab012_If_Else {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+
+        if (age > 18){
+            System.out.println("You can drive!");
+        }
+        else {
+            System.out.println("You can't drive!");
+        }
+
+    }
+}
