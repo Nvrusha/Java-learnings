53dd29ab863147e5b9268883c42e7d663a6e7784
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-10-30 12:58:42 +0530

    New code implemented

--- src/Oct/Ex10_Loops/Lab017_Practice.java
+++ src/Oct/Ex10_Loops/Lab017_Practice.java
@@ -1,4 +1,21 @@
 package Oct.Ex10_Loops;
 
 public class Lab017_Practice {
-}
+    public static void main(String[] args) {
+        // 1. Int data type
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+
+        // 2. Double data type
+        for (double i = 0.0; i < 1.0; i += 0.1) {
+            System.out.printf("Value: %.1f%n", i);
+        }
+
+        // 3. Character Data Type
+        for (char c = 'a'; c <= 'z'; c++) {
+            System.out.println("Character: " + c);
+        }
+        }
+    }
+
