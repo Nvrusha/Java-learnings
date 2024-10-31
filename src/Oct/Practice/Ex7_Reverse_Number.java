1a18cd0f853f4d5c7df02cdcc7031e7bbfb4b58c
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-10-31 21:59:55 +0530

    New code implemented

---
+++ src/Oct/Practice/Ex7_Reverse_Number.java
@@ -0,0 +1,11 @@
+package Oct.Practice;
+
+public class Ex7_Reverse_Number {
+    public static void main(String[] args) {
+        //Reverse the number using for loop. (In - 12345, Out - 54321)
+
+        int num = 12345;
+        int rev = 0;
+
+    }
+}
