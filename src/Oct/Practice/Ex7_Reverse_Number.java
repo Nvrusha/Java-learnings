e3788c059f65295e1eaa5d9a78e38c4c4da2d488
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-11-02 14:13:34 +0530

    New code implemented

--- src/Oct/Practice/Ex7_Reverse_Number.java
+++ src/Oct/Practice/Ex7_Reverse_Number.java
@@ -2,6 +2,27 @@ package Oct.Practice;
 
 public class Ex7_Reverse_Number {
     public static void main(String[] args) {
+        //Reverse the number using for loop. (In - 12345, Out - 54321)
+        int num1 = 123456789;
+        int rev1 = 0;
+
+        for(;num1 != 0;){
+            int lastDigit1 = num1 % 10;
+            rev1 = rev1 * 10 + lastDigit1;
+            num1 /= 10;
+        }
+        System.out.println("Reversed number: " + rev1);
+
+
+
+
+
+
+
+
+
+
+
         //Reverse the number using while loop. (In - 12345, Out - 54321)
 
         int num = 12345;   // Original number we want to reverse
@@ -13,6 +34,8 @@ public class Ex7_Reverse_Number {
             num = num / 10;                // Remove the last digit from num
         }
         System.out.println("Reversed number: " + rev);
+
+
 
     }
 }
