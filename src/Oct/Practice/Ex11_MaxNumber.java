0e30cc0751da20c7df0ef22d030d6983c5f61329
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-11-10 17:29:30 +0530

    New code implemented

--- src/Oct/Practice/Ex11_MaxNumber.java
+++ src/Oct/Practice/Ex11_MaxNumber.java
@@ -4,19 +4,25 @@ public class Ex11_MaxNumber {
     public static void main(String[] args) {
         // Find maximum number in an array
 
-        int [] array = {130,7,4,90,23,54,23,89,110};
+        int[] array = {130, 7, 4, 90, 23, 54, 23, 89, 110};
 
+        // Call the max_Number method and store the result in max_value
+        int max_value = max_Number(array);
+
+        System.out.println("The maximum number is " + max_value);
+    }
+
+    // Method to find the maximum number in an array
+    static int max_Number(int[] array) {
         // Initialize max to the first element of the array
         int max = array[0];
 
-        // Loop through the array to find the maximum
-        for (int i = 0; i < array.length; i++ ){
-            if(array[i] > max){
+        for (int i = 1; i < array.length; i++) { // Start from index 1 as max is already array[0]
+            if (array[i] > max) {
                 max = array[i];  // Update max if current element is greater
             }
         }
-
-        // Print the maximum value found
-        System.out.println(max);
+        // Return the maximum value found
+        return max;
     }
 }
