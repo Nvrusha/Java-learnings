ceda64c19050fa17893645e39ae17e4a33d6d641
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-11-10 22:55:31 +0530

    New code implemented

--- src/Oct/Practice/Ex12_FindDuplicates.java
+++ src/Oct/Practice/Ex12_FindDuplicates.java
@@ -17,11 +17,13 @@ public class Ex12_FindDuplicates {
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
 
+        // Step 2: Loop through the sorted array and count consecutive duplicates
         for (int i = 0; i < array.length-1; i++) {
-            if(array[i+1] == array[i]){
+            if(array[i+1] == array[i]){     // Check if the current element is the same as the next one
                 countDuplicates++;
             }
         }
+        // Step 3: Return the total count of duplicate occurrences
         return countDuplicates;
     }
 }
