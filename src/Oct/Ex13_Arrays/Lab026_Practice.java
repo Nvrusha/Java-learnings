8cdcbe829c76abda140f411867bdf51b9b810487
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-11-10 22:58:04 +0530

    New code implemented

--- src/Oct/Ex13_Arrays/Lab026_Practice.java
+++ src/Oct/Ex13_Arrays/Lab026_Practice.java
@@ -9,18 +9,13 @@ public class Lab026_Practice {
 
         //Purpose: Searches for a specified value in a sorted array and returns its index,
         //or -1 if not found.
+
         //Note: The array must be sorted before calling binarySearch.
 
         int [] array = {102,98,34,67,5,90,23,7,8};
 
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
-
         System.out.println(Arrays.binarySearch(array, 9));
-
-
-
-
-
     }
 }
