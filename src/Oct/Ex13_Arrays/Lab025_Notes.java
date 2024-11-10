8efb6a35cc613717c4cda70d6f40f12de5f7f23a
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-11-10 16:23:24 +0530

    New code implemented

--- src/Oct/Ex13_Arrays/Lab025_Notes.java
+++ src/Oct/Ex13_Arrays/Lab025_Notes.java
@@ -1,4 +1,9 @@
 package Oct.Ex13_Arrays;
 
+import java.util.Arrays;
+
 public class Lab025_Notes {
+    public static void main(String[] args) {
+
+    }
 }
--- src/Oct/Ex13_Arrays/Lab026_Practice.java
+++ src/Oct/Ex13_Arrays/Lab026_Practice.java
@@ -1,4 +1,26 @@
 package Oct.Ex13_Arrays;
 
+import java.util.Arrays;
+
 public class Lab026_Practice {
+    public static void main(String[] args) {
+
+        // Arrays.binarySearch(array, value)
+
+        //Purpose: Searches for a specified value in a sorted array and returns its index,
+        //or -1 if not found.
+        //Note: The array must be sorted before calling binarySearch.
+
+        int [] array = {102,98,34,67,5,90,23,7,8};
+
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+        System.out.println(Arrays.binarySearch(array, 9));
+
+
+
+
+
+    }
 }
