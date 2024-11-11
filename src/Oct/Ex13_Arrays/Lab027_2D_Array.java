ec03a583b091d484d9134a6a8ff700391a0b1300
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-11-11 16:19:54 +0530

    New code implemented

---
+++ src/Oct/Ex13_Arrays/Lab027_2D_Array.java
@@ -0,0 +1,30 @@
+package Oct.Ex13_Arrays;
+
+import java.util.Arrays;
+
+public class Lab027_2D_Array {
+    public static void main(String[] args) {
+        // Declare and initialize a 2D array with 3 rows and 3 columns
+        int [][] array = new int[3][3];
+
+        // Assign values to the first row
+        array[0][0] = 90;
+        array[0][1] = 91;
+        array[0][2] = 92;
+
+        // Assign values to the second row
+        array[1][0] = 80;
+        array[1][1] = 81;
+        array[1][2] = 82;
+
+        // Assign values to the third row
+        array[2][0] = 70;
+        array[2][1] = 71;
+        array[2][2] = 72;
+
+        // Print the 2D array using Arrays.deepToString to display it in a readable format
+        System.out.println(Arrays.deepToString(array));
+
+
+    }
+}
