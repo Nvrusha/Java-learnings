4d354f106d0bbe11bb23b82246a3b42e1e34a5f5
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-12-04 08:56:36 +0530

    New code implemented

---
+++ src/Oct/Ex4_Variable_Datatype/Types/Local_Variables.java
@@ -0,0 +1,15 @@
+package Oct.Ex4_Variable_Datatype.Types;
+
+public class Local_Variables {
+    public static void main(String[] args) {
+
+        //Declared inside a method, constructor, or block.
+        //Scope: Limited to the method, constructor, or block in which they are defined.
+        //Must be initialized before use.
+        //Not accessible outside their scope.
+        //Memory: Stored in the stack.
+
+        int num = 10; // Local variable
+        System.out.println("Local variable: " + num);
+    }
+}
